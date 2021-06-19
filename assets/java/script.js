06e6155df5cbd4ba221d16e4a284882b9490779f
Tyler Starling <tyler.a.starling@gmail.com>
2021-06-19 15:27:51 -0400

    Made some tweaks to remove previous event listeners from following event listeners as they were interfering with one another.

--- assets/java/script.js
+++ assets/java/script.js
@@ -20,34 +20,71 @@ var optionB = document.getElementById('optionB');
 var optionC = document.getElementById('optionC');
 var optionD = document.getElementById('optionD');
 
+function starterPosition() {
+
+    optionA.style.visibility = "visible";
+    optionB.style.visibility = "hidden";
+    optionC.style.visibility = "hidden";
+    optionD.style.visibility = "hidden";
+    question.style.fontSize = "x-large";
+    question.innerHTML = "Click to start!";
+    optionA.innerHTML = "Start"
+
+
+    optionA.addEventListener("click", displayQuestion1);
+    
+
+    return;
+}
+
 function displayQuestion1() {
+    optionA.removeEventListener("click", displayQuestion1);
+
+    optionA.style.visibility = "visible";
+    optionB.style.visibility = "visible";
+    optionC.style.visibility = "visible";
+    optionD.style.visibility = "visible";
     question.innerHTML = quesList[0].question;
     optionA.innerHTML = quesList[0].option[0];
     optionB.innerHTML = quesList[0].option[1];
     optionC.innerHTML = quesList[0].option[2];
     optionD.innerHTML = quesList[0].option[3];
     
+
     optionC.addEventListener("click", displayQuestion2);
+
+    return;
 }
 
 function displayQuestion2() {
+    optionC.removeEventListener("click", displayQuestion2);
+
     question.innerHTML = quesList[1].question;
     optionA.innerHTML = quesList[1].option[0];
     optionB.innerHTML = quesList[1].option[1];
     optionC.innerHTML = quesList[1].option[2];
     optionD.innerHTML = quesList[1].option[3];
 
+
     optionB.addEventListener("click", displayQuestion3);
+
+    return;
 }
 
 function displayQuestion3() {
+
+    optionB.removeEventListener("click", displayQuestion3);
+
     question.innerHTML = quesList[2].question;
     optionA.innerHTML = quesList[2].option[0];
     optionB.innerHTML = quesList[2].option[1];
     optionC.innerHTML = quesList[2].option[2];
     optionD.innerHTML = quesList[2].option[3];
 
+
     optionA.addEventListener("click", winner);
+
+    return;
 }
 
 function winner() {
@@ -57,9 +94,12 @@ function winner() {
     optionD.style.visibility = "hidden";
     question.style.fontSize = "x-large";
     question.innerHTML = "YOU WIN!";
+
+    stopImmediatePropagation();
+    return;
 }
 
-displayQuestion1();
+starterPosition();
 
 
 
