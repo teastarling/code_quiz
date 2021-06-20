070b419d23944a200bd491f0eb75b53a3d316e67
Tyler Starling <tyler.a.starling@gmail.com>
2021-06-20 11:08:53 -0400

    created function with algorithm so answers can run without using separate functions for each. used onclick in html in order to reduce eventListener conflict issues. beginning to set up right and wrong answer functions to keep score and penalize time when they are implemented.

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,103 +1,117 @@
-// Question objects
-
-var quesList = [ {
-        question:'The right answer is red',
-        option: ['a.)Blue','b.)Green','c.)Red','d.)Orange']
-    },
-    {   question: 'The right answer is elephant',
-        option: ['a.)Kangaroo','b.)Elephant','c.)Lizard','d.)Monkey']
-    },
-    {
-        question: 'The right answer is Africa',
-        option: ['a.)Africa','b.)India','c.)Russia','d.)Australia']
-    }
-]
-
 var question = document.getElementById('question');
-var questionContainer = document.getElementById('questionContainer');
+var quizContainer = document.getElementById('quizContainer');
 var optionA = document.getElementById('optionA');
 var optionB = document.getElementById('optionB');
 var optionC = document.getElementById('optionC');
 var optionD = document.getElementById('optionD');
 
+var starter = document.getElementById('starter');
+var startTitle = document.getElementById('startTitle');
+var startBtn = document.getElementById('startBtn');
+
+
+// Question object
+var quesList = [ {
+        question:'The right answer is red',
+        option: ['a.)Blue','b.)Green','c.)Red','d.)Orange'],
+        answer: "c"
+    },
+    {   question: 'The right answer is elephant',
+        option: ['a.)Kangaroo','b.)Elephant','c.)Lizard','d.)Monkey'],
+        answer: "b"
+    },
+    {
+        question: 'The right answer is Africa',
+        option: ['a.)Africa','b.)India','c.)Russia','d.)Australia'],
+        answer: "a"
+    }
+]
+
+// var score = 0;
+var currentQuestion = 0;
+
+
+
+
 function starterPosition() {
 
-    optionA.style.visibility = "visible";
-    optionB.style.visibility = "hidden";
-    optionC.style.visibility = "hidden";
-    optionD.style.visibility = "hidden";
-    question.style.fontSize = "x-large";
-    question.innerHTML = "Click to start!";
-    optionA.innerHTML = "Start"
-
-
-    optionA.addEventListener("click", displayQuestion1);
-    
-
-    return;
-}
-
-function displayQuestion1() {
-    optionA.removeEventListener("click", displayQuestion1);
-
-    optionA.style.visibility = "visible";
-    optionB.style.visibility = "visible";
-    optionC.style.visibility = "visible";
-    optionD.style.visibility = "visible";
-    question.innerHTML = quesList[0].question;
-    optionA.innerHTML = quesList[0].option[0];
-    optionB.innerHTML = quesList[0].option[1];
-    optionC.innerHTML = quesList[0].option[2];
-    optionD.innerHTML = quesList[0].option[3];
-    
-
-    optionC.addEventListener("click", displayQuestion2);
-
-    return;
-}
-
-function displayQuestion2() {
-    optionC.removeEventListener("click", displayQuestion2);
-
-    question.innerHTML = quesList[1].question;
-    optionA.innerHTML = quesList[1].option[0];
-    optionB.innerHTML = quesList[1].option[1];
-    optionC.innerHTML = quesList[1].option[2];
-    optionD.innerHTML = quesList[1].option[3];
-
-
-    optionB.addEventListener("click", displayQuestion3);
-
-    return;
-}
-
-function displayQuestion3() {
-
-    optionB.removeEventListener("click", displayQuestion3);
-
-    question.innerHTML = quesList[2].question;
-    optionA.innerHTML = quesList[2].option[0];
-    optionB.innerHTML = quesList[2].option[1];
-    optionC.innerHTML = quesList[2].option[2];
-    optionD.innerHTML = quesList[2].option[3];
-
-
-    optionA.addEventListener("click", winner);
-
-    return;
-}
-
-function winner() {
+    question.style.visibility = "hidden";
     optionA.style.visibility = "hidden";
     optionB.style.visibility = "hidden";
     optionC.style.visibility = "hidden";
     optionD.style.visibility = "hidden";
-    question.style.fontSize = "x-large";
-    question.innerHTML = "YOU WIN!";
+    startTitle.style.visibility = "visible";
+    startBtn.style.visibility = "visible";
 
-    stopImmediatePropagation();
+    startTitle.style.fontSize = "x-large";
+    startTitle.innerHTML = "Click to start!";
+    startBtn.innerHTML = "Start"
+
+
+    startBtn.addEventListener("click", displayQuestion);
+    
+
     return;
 }
+
+function displayQuestion() {
+    var q = quesList[currentQuestion];
+    console.log(currentQuestion);
+
+    question.style.visibility = "visible";
+    optionA.style.visibility = "visible";
+    optionB.style.visibility = "visible";
+    optionC.style.visibility = "visible";
+    optionD.style.visibility = "visible";
+    startTitle.style.visibility = "hidden";
+    startBtn.style.visibility = "hidden";
+
+    question.innerHTML = q.question;
+    optionA.innerHTML = q.option[0];
+    optionB.innerHTML = q.option[1];
+    optionC.innerHTML = q.option[2];
+    optionD.innerHTML = q.option[3];
+
+    return;
+}
+
+
+function winner() {
+    question.style.visibility = "hidden";
+    optionA.style.visibility = "hidden";
+    optionB.style.visibility = "hidden";
+    optionC.style.visibility = "hidden";
+    optionD.style.visibility = "hidden";
+    startTitle.style.visibility = "visible";
+    startBtn.style.visibility = "visible";
+
+    startTitle.style.fontSize = "x-large";
+    startTitle.innerHTML = "YOU WIN!";
+
+    return;
+}
+
+function answerCheck(x) {
+    if ((x === quesList[currentQuestion].answer) && (currentQuestion < (quesList.length - 1))) {
+        answerCorrect();
+        console.log("more questions");
+    } else if ((x === quesList[currentQuestion].answer) && (currentQuestion === (quesList.length - 1))) {
+        console.log("a winner is you");
+        winner();
+    } //else {
+    //     answerWrong()
+    // }
+}
+console.log(currentQuestion)
+
+function answerCorrect() {
+    currentQuestion++;
+    displayQuestion();
+}
+
+// function answerWrong () {
+//     timer - 10
+// }
 
 starterPosition();
 
